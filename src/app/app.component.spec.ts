4aa135a0dd4b14519aa0d302568fa76dfaec91a6
eder6 <eder@b2mlportal.com.br>
2021-09-02 14:24:28 -0300

    primeiro commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'gerenciador-de-tarefas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gerenciador-de-tarefas');
+    expect('').toEqual('gerenciador-de-tarefas');
   });
 
   it('should render title', () => {
